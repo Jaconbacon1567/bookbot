509e45fc29acf8c01d5e9517330040e254b5bd2e
Jaconbacon1567 <anthonyjfeola@gmail.com>
2024-10-21 16:44:26 -0400

    sorting chars

--- main.py
+++ main.py
@@ -2,7 +2,29 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(num_words)
+    chars_dict = get_num_chars(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_chars(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if(lowered in chars):
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_num_words(text):
@@ -13,4 +35,15 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
+def sort_on(dict):
+    return dict["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
 main()
